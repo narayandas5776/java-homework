e4d76df007504a8363bd410f46e5d13e3dffed40
narayandas5776 <nanodas5776@gmail.com>
2024-07-10 21:14:40 +0530

    swap2number.java

---
+++ swap2number.java
@@ -0,0 +1,12 @@
+public class userin{
+    public static void main(String[] args){
+        int a= 3;
+        int b = 4;
+        int temp = 0;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
